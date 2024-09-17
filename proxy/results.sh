d295cd094b3cba0f02a8b56934d851022bb653d5
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2024-09-17 11:38:46 -0400

    Update results.sh

--- proxy/results.sh
+++ proxy/results.sh
@@ -6,6 +6,4 @@ cat proxy.log
 
 echo ### STORAGE ###
 
-tree /home/runner/work/verdaccio-proxy-config-test-case/verdaccio-proxy-config-test-case/proxy/storage/ >storage.log
-
-cat storage.log
+tree ./storage/ 
