4fa89d2a9db7828b9776270a77d36fdeb3145a35
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2024-09-23 13:13:17 -0400

    Update

--- proxy/proxy.sh
+++ proxy/proxy.sh
@@ -3,10 +3,12 @@
 TEST_CASE_PROXY=$1
 
 rm proxy.log
+rm debug.log 
+rm error.log
 
 # Boot the server in a background process.
 # nohup npx verdaccio@latest --config ./config.yml --listen $TEST_CASE_PROXY &
-DEBUG="verdaccio:storage,verdaccio:proxy" nohup npx verdaccio@next-7 --config ./config.yml --listen $TEST_CASE_PROXY > debug.log 2> error.log &
+nohup DEBUG="verdaccio:storage,verdaccio:proxy" npx verdaccio@next-7 --config ./config.yml --listen $TEST_CASE_PROXY >debug.log 2>error.log &
 
 # Wait for the server to begin listening for connections
 ( tail -F -n10 proxy.log & ) | fgrep -q $TEST_CASE_PROXY
