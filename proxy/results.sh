19728540d213bbd697dd0092c2c01b931ab0dd3b
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2024-09-23 15:53:41 -0400

    Update

--- proxy/results.sh
+++ proxy/results.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
+echo "###"
 echo "### LOG ###"
+echo "###"
 
 cat proxy.log
 
+echo "###"
 echo "### DEBUG ###"
+echo "###"
 
 cat debug.log
 
+echo "###"
 echo "### ERRORS ###"
+echo "###"
 
 cat error.log
 
+echo "###"
 echo "### STORAGE ###"
+echo "###"
 
-# tree ./storage/ 
-ls -lR ./storage/
+tree -s ./storage/ 
+# ls -lR ./storage/
+
+echo "###"
+echo "### EXAMPLE @types/yargs-parser ###"
+echo "###"
+
+cat ./storage/@types/yargs-parser/package.json
+
+echo "###"
+echo "### EXAMPLE yargs-parser ###"
+echo "###"
+
+cat ./storage/yargs-parser/package.json
