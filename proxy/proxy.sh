f2a593a07ad23d2d2d4f8d9de3d7aa5e75657df8
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2024-09-28 17:54:55 -0400

    No --listen

--- proxy/proxy.sh
+++ proxy/proxy.sh
@@ -6,7 +6,8 @@ VERSION=$2
 rm *.log
 
 # Boot the server in a background process.
-nohup env DEBUG="express:*,verdaccio:*" npx verdaccio@$VERSION --config ./config.yml --listen $TEST_CASE_PROXY >out.log 2>debug.log &
+# nohup env DEBUG="express:*,verdaccio:*" npx verdaccio@$VERSION --config ./config.yml --listen $TEST_CASE_PROXY >out.log 2>debug.log &
+nohup npx verdaccio@$VERSION --config ./config.yml &
 
 # Wait for the server to begin listening for connections
 ( tail -F -n10 proxy.log & ) | fgrep -q $TEST_CASE_PROXY
