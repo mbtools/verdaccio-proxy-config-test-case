d241345e33ce39dc5f703e4f3aa92db5ec097751
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2024-10-10 09:48:05 -0400

    debug

--- proxy/proxy.sh
+++ proxy/proxy.sh
@@ -3,12 +3,10 @@
 TEST_CASE_PROXY=$1
 VERSION=$2
 
-npm config set prefer-online true
-npm config set registry $TEST_CASE_PROXY
+rm -f *.log
 
 # Boot the server in a background process.
-# nohup env DEBUG="express:*,verdaccio:*" npx verdaccio@$VERSION --config ./config.yml --listen $TEST_CASE_PROXY >out.log 2>debug.log &
-nohup env DEBUG="verdaccio:*" npx verdaccio@$VERSION --config ./config.yml >out.log 2>debug.log &
+nohup env DEBUG="verdaccio:*" npx verdaccio@$VERSION --config ./config.yml > debug.log 2>&1 &
 
 # Wait for the server to begin listening for connections
 ( tail -F -n10 proxy.log & ) | fgrep -q $TEST_CASE_PROXY
