4b9538d5c78bc3a8390b98865c5e625e1df6aca6
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2024-10-10 11:33:24 -0400

    always-auth

--- proxy/proxy.sh
+++ proxy/proxy.sh
@@ -2,14 +2,12 @@
 
 TEST_CASE_PROXY=$1
 
-rm proxy.log
-rm debug.log 
-rm error.log
+rm -f *.log
 
 # Boot the server in a background process.
 # nohup npx verdaccio@latest --config ./config.yml --listen $TEST_CASE_PROXY &
 # nohup env DEBUG="express:*,verdaccio:storage*,verdaccio:proxy*,verdaccio:plugin:local-storage*,verdaccio:api*" npx verdaccio@next-7 --config ./config.yml --listen $TEST_CASE_PROXY >debug.log 2>error.log &
-nohup env DEBUG="verdaccio:*,-verdaccio:core:*" npx verdaccio@8.0.0-next-8.3 --config ./config.yml --listen $TEST_CASE_PROXY >debug.log 2>error.log &
+nohup env DEBUG="verdaccio:*,-verdaccio:core:*" npx verdaccio@8.0.0-next-8.3 --config ./config.yml >debug.log 2>error.log &
 
 # Wait for the server to begin listening for connections
 ( tail -F -n10 proxy.log & ) | fgrep -q $TEST_CASE_PROXY
